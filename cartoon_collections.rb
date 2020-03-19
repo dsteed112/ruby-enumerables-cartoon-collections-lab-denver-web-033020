3a7a13b1bbacb9a8910be230ecc2cf1d75c96899
Daniel Steed <dsteed112@gmail.com>
2020-03-19 18:57:07 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -4,8 +4,11 @@ def roll_call_dwarves(dwarves)
   end
 end
 
-def summon_captain_planet# code an argument here
-  # Your code here
+def summon_captain_planet(forces)
+    # Your code here
+  # planeteer_calls = ["earth", "wind", "fire", "water", "heart"]
+    veggies.collect do |call| 
+    call.capitalize + "!"
 end
 
 def long_planeteer_calls# code an argument here
